8abef290eb53840006c6a622ca45f6e99591d0d6
joeinman <joe.inman8@gmail.com>
2024-08-20 16:08:13 +0100

    Made lane drawing more efficient

--- src/Simulate.py
+++ src/Simulate.py
@@ -46,6 +46,28 @@ def main():
     car_img = pygame.transform.rotate(car_img, -90)
     car_img = pygame.transform.scale(car_img, (CAR_RADIUS*3, CAR_RADIUS*2))
 
+    # Create Lane Transparent Surface
+    lane_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+    for motorway in lane_y_positions:
+
+        # For Each Lane
+        for y in motorway:
+
+            # Draw The Lane Background
+            pygame.draw.rect(lane_surface, (70, 70, 70), (0, y - LANE_HEIGHT//2, SCREEN_WIDTH, LANE_HEIGHT))
+
+            # Draw the motorway center line as dashes
+            for x in range(0, SCREEN_WIDTH, 20):
+                pygame.draw.line(lane_surface, (*WHITE, 120), (x, y), (x + 10, y), 2)
+            
+            # Draw each lane border
+            pygame.draw.line(lane_surface, WHITE, (0, y - LANE_HEIGHT//2), (SCREEN_WIDTH, y - LANE_HEIGHT//2), 2)
+            pygame.draw.line(lane_surface, WHITE, (0, y + LANE_HEIGHT//2), (SCREEN_WIDTH, y + LANE_HEIGHT//2), 2)
+
+        # Draw the border across all lanes
+        pygame.draw.line(lane_surface, WHITE, (0, motorway[0] - LANE_HEIGHT//2), (SCREEN_WIDTH, motorway[0] - LANE_HEIGHT//2), 8)
+        pygame.draw.line(lane_surface, WHITE, (0, motorway[2] + LANE_HEIGHT//2), (SCREEN_WIDTH, motorway[2] + LANE_HEIGHT//2), 8)
+
     # Main loop
     running = True
     while running:
@@ -58,25 +80,7 @@ def main():
         lanes = evolve(lanes, 0.05, 100)
 
         # Draw The Lanes
-        for motorway in lane_y_positions:
-
-            # For Each Lane
-            for y in motorway:
-
-                # Draw The Lane Background
-                pygame.draw.rect(screen, (70, 70, 70), (0, y - LANE_HEIGHT//2, SCREEN_WIDTH, LANE_HEIGHT))
-
-                # Draw the motorway center line as dashes
-                for x in range(0, SCREEN_WIDTH, 20):
-                    pygame.draw.line(screen, WHITE, (x, y), (x + 10, y), 2)
-                
-                # Draw each lane border
-                pygame.draw.line(screen, WHITE, (0, y - LANE_HEIGHT//2), (SCREEN_WIDTH, y - LANE_HEIGHT//2), 2)
-                pygame.draw.line(screen, WHITE, (0, y + LANE_HEIGHT//2), (SCREEN_WIDTH, y + LANE_HEIGHT//2), 2)
-
-            # Draw the border across all lanes
-            pygame.draw.line(screen, WHITE, (0, motorway[0] - LANE_HEIGHT//2), (SCREEN_WIDTH, motorway[0] - LANE_HEIGHT//2), 8)
-            pygame.draw.line(screen, WHITE, (0, motorway[2] + LANE_HEIGHT//2), (SCREEN_WIDTH, motorway[2] + LANE_HEIGHT//2), 8)
+        screen.blit(lane_surface, (0, 0))
 
         # Draw The Cars
         for i in range(3):
